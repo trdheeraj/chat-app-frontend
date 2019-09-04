dffbfefff6c986e9d61b4fc0cfb454cd6ba09768
tdheerj94 <dheeraj.tr@weinvest.net>
2019-09-05 01:41:19 +0530

    First commit

---
+++ build/precache-manifest.5dda094d64f242222d08ed41d61ad7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48640ddb8f0242b4d9c2d7b771a1681e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f3e34437a741895801a",
+    "url": "/static/css/2.5bbf332f.chunk.css"
+  },
+  {
+    "revision": "9e6600ae331e7ed758a1",
+    "url": "/static/css/main.5ce3f41a.chunk.css"
+  },
+  {
+    "revision": "6f3e34437a741895801a",
+    "url": "/static/js/2.50ef08b9.chunk.js"
+  },
+  {
+    "revision": "9e6600ae331e7ed758a1",
+    "url": "/static/js/main.f0a244a4.chunk.js"
+  },
+  {
+    "revision": "0dc860c1e42c217d27a7",
+    "url": "/static/js/runtime~main.eb670399.js"
+  }
+]);
